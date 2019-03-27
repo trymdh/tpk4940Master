6f7dec5424441852d15139add1fadcf1d74c868a
Trym Dyrkolbotn Hauglund <44025792+trymdh@users.noreply.github.com>
2019-03-27 13:18:27 +0100

    Requested by Espen

---
+++ Python/VS Code/continuerlig_LaserImage.py
@@ -0,0 +1,19 @@
+from AT_cx_functions import*
+from matplotlib import pyplot as plt
+
+#Find and configure the device
+hDev = getDevice()
+cogConfig(hDev)
+
+plt.gca().invert_yaxis()
+for i in range(0,1000):
+
+    #Snap an image of the scene in COG mode.
+    laserlinepixels = snap(hDev)
+    #reshape the COG-image from (2048,) to (2048,2)
+    laserpixel = pixCoordify(laserlinepixels.ravel() / 64 , 2048)
+
+    plt.plot(laserpixel)
+    plt.pause(1/(3*60))
+
+plt.show()
